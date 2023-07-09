e8ab4d7b4cf4e485ce0147e21b8f04ed7d00a1a3
Ben Woodward <ben@benwoodward.me.uk>
2023-07-09 17:37:59 +0100

    Configure prettier with eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -16,7 +18,10 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: [
+    'prettier',
+    'react-refresh'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
